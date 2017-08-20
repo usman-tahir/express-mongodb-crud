a5eedb4d018fda16bb07c9959080971f7deff3c6
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-08-20 14:18:11 -0400

    added a default route that accesses an index.html file

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const express = require('express'),
 app.listen(3000, function() {
     console.log('Listening at http://localhost:3000');
 });
+
+// Default path
+app.get('/', function (req, res) {
+    // res.send('Hello World');
+    res.sendFile(__dirname + '/index.html');
+});
