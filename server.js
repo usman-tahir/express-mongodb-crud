f0e63144ce3669bfd0b38a04fb0a4a3d3ec53e1b
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-08-20 14:59:25 -0400

    added the mongodb dependency; set up a connection with the mLab database that was created

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 
 const express = require('express'),
     bodyParser = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
     app = express();
+
+var database;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function() {
-    console.log('Listening at http://localhost:3000');
+// Set up a connection to the MongoDB Client
+MongoClient.connect('mongodb://root:root@ds153113.mlab.com:53113/express-mongodb-crud', function (err, db) {
+    if (err) {
+        return console.log(err);
+    } else {
+        database = db;
+        app.listen(3000, function() {
+            console.log('Connected to the mLab database.');
+            console.log('Listening at http://localhost:3000');
+        });
+    }
 });
 
 // Default path
